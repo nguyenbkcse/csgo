3cfb633226103686734bb77c9d64564269542bf6
nguyenbkcse <nguyenbkcse@gmail.com>
2021-05-28 11:12:04 +0700

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -71,7 +71,7 @@ bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
 			-ip "${SRCDS_IP}" \
 			+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
 			+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
-      +host_workshop_map ${SRCDS_HOST_WORKSHOP_MAP} \
+			+host_workshop_map ${SRCDS_HOST_WORKSHOP_MAP} \
 			-authkey "${SRCDS_WORKSHOP_AUTHKEY}" \
 			"${ow_args}" \
 			"${ADDITIONAL_ARGS}"
